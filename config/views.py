9bb2a9ace892737b116684d8ea82d0d419b54f62
wooookim <jhr01175@naver.com>
2024-03-27 17:26:45 +0900

    first

---
+++ config/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from burgers.models import Burger
+
+
+def main(request):
+    return render(request, "main.html")
+
+
+def burger_list(request):
+    burgers = Burger.objects.all()
+    print("전체 버거 목록: ", burgers)
+
+    context = {
+        "burgers": burgers,
+    }
+    return render(request, "burger_list.html", context)
+
+
+def burger_search(request):
+    keyword = request.GET.get("keyword")
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    else:
+        burgers = Burger.objects.none()
+
+    context = {
+        "burgers": burgers,
+    }
+
+    return render(request, "burger_search.html", context)
